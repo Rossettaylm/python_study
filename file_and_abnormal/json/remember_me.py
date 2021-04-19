e22228a3e88c164b455340c96837bfc531cf1b82
Rossettaylm <2441197035@qq.com>
2021-04-19 11:18:54 +0800

    qeushi

--- file_and_abnormal/json/remember_me.py
+++ file_and_abnormal/json/remember_me.py
@@ -1,8 +1,28 @@
 import json
 
-username = input("What is your name? ")
-filename = 'username.json'
-with open(filename, 'w') as file_obj:
-    json.dump(username, file_obj)
-    print("We'll remember you when you come back, " + username + "!")
+def get_stored_username():
+    filename = 'username.json'
+    try:
+        with open(filename) as file_obj:
+            username = json.load(file_obj)
+    except FileNotFoundError:
+        return None
+    else:
+        return username
 
+def get_new_username():
+    username = input("What is your name? ")
+    filename = 'username.json'
+    with open(filename, 'w') as file_obj:
+        json.dump(username,file_obj)
+    return username
+
+def greet_user():
+    username = get_stored_username()
+    if username:
+        print("Welcome back, " + username + "!")
+    else:
+        username = get_new_username()
+        print("We'll remember you when you come back, " + username + "!")
+
+greet_user()
