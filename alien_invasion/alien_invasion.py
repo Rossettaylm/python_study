dc4bb4ed1481a03448a531c978bb4fe9ec6790d9
Rossettaylm <2441197035@qq.com>
2021-04-20 12:26:06 +0800

    commit

--- alien_invasion/alien_invasion.py
+++ alien_invasion/alien_invasion.py
@@ -1,6 +1,7 @@
 import sys
 import pygame
 from settings import Settings 
+from ship import Ship
 
 def run_game():
     pygame.init()
@@ -11,13 +12,17 @@ def run_game():
     pygame.display.set_caption("Alien Invasion")
     #设置背景颜色
     bg_color = (ai_settings.bg_color)
+    #创建一艘飞船
+    ship = Ship(screen)
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
-        screen.fill(bg_color)
+        screen.fill(ai_settings.bg_color)
+        ship.blitme()
 
+        #让最近绘制的屏幕可见
         pygame.display.flip()
 
 run_game()
