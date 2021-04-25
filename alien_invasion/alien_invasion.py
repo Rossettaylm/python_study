b2ca96090c517ccc6e291204267febed279f02c5
Rossettaylm <2441197035@qq.com>
2021-04-25 21:54:14 +0800

    keyi

--- alien_invasion/alien_invasion.py
+++ alien_invasion/alien_invasion.py
@@ -1,28 +1,33 @@
-import sys
 import pygame
-from settings import Settings 
+from pygame.sprite import Group
+import game_functions as gf
+from settings import Settings
 from ship import Ship
+
 
 def run_game():
     pygame.init()
     ai_settings = Settings()
     screen = pygame.display.set_mode((ai_settings.screen_width,
-        ai_settings.screen_height))
-    #创建标题
+                                      ai_settings.screen_height))
+    # 创建标题
     pygame.display.set_caption("Alien Invasion")
-    #设置背景颜色
-    bg_color = (ai_settings.bg_color)
-    #创建一艘飞船
-    ship = Ship(screen)
+    # 设置背景颜色
+    bg_color = ai_settings.bg_color
+    # 创建一艘飞船，一个子弹编组和一个外星人编组
+    ship = Ship(ai_settings, screen)
+    bullets = Group()
+    aliens = Group()
+
+    # 创建外星人群
+    gf.create_fleet(ai_settings, screen, aliens)
 
     while True:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                sys.exit()
-        screen.fill(ai_settings.bg_color)
-        ship.blitme()
+        gf.check_events(ai_settings, screen, ship, bullets)
+        ship.update()
+        bullets.update()
+        gf.update_bullets(bullets)
+        gf.update_screen(ai_settings, screen, ship, aliens, bullets)
 
-        #让最近绘制的屏幕可见
-        pygame.display.flip()
 
 run_game()
