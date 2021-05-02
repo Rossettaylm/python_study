2362be704518cfeda60f111a95ed416214a5f4f7
Rossettaylm <2441197035@qq.com>
2021-05-02 20:30:41 +0800

    finish chapter 16

--- data_visual/csv_json/csv_weather/highs_lows.py
+++ data_visual/csv_json/csv_weather/highs_lows.py
@@ -4,7 +4,7 @@ from datetime import datetime
 
 
 # 从文件中获取日期，最高气温, 最低气温
-filename = 'sitka_weather_2014.csv'
+filename = 'death_valley_2014.csv'
 with open(filename) as f:
     reader = csv.reader(f)
     # 阅读器对象返回下一行，即第一行
@@ -17,14 +17,16 @@ with open(filename) as f:
     dates, highs, lows = [], [], []
     # 阅读器对象将从第二行开始遍历
     for row in reader:
-        current_date = datetime.strptime(row[0], "%Y-%m-%d")
-        dates.append(current_date)
-
-        high = int(row[1])
-        highs.append(high)
-
-        low = int(row[3])
-        lows.append(low)
+        try:
+            current_date = datetime.strptime(row[0], "%Y-%m-%d")
+            high = int(row[1])
+            low = int(row[3])
+        except ValueError:
+            print(current_date, 'missing data')
+        else:
+            dates.append(current_date)
+            highs.append(high)
+            lows.append(low)
 
 
 # 根据数据绘制图形
@@ -37,7 +39,7 @@ plt.fill_between(dates, highs, lows,
                 facecolor='blue', alpha=0.1)
 
 # 设置图形的格式
-plt.title("Daily high and low temperatures - 2014", fontsize=24)
+plt.title("Daily high and low temperatures - 2014\nDeath Valley, CA", fontsize=24)
 plt.xlabel('', fontsize=16)
 plt.ylabel("Temperature (F)", fontsize=16)
 
