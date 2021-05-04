78e84c943fda9496e6589409f93052113e2a5f3e
Rossettaylm <2441197035@qq.com>
2021-05-04 15:45:57 +0800

    new dir: python_official

---
+++ base/python_official/list.py
@@ -0,0 +1,27 @@
+# (1) 列表支持合并操作
+cubes = [1, 4, 9]
+cubes = cubes + [16, 25]
+print(cubes)
+
+# (2) 为切片赋值可以改变列表大小，甚至清空整个列表
+letters = ['a', 'b', 'c', 'd']
+print(len(letters))
+
+letters[1:3] = ['B', 'C'] # replace
+print(letters)
+
+letters[1:3] = [] # delete
+print(letters)
+
+letters[:] = [] # clear
+print(letters)
+
+# (3) 关键字参数 end 可以取消输出后面的换行, 或用另一个字符串结尾
+a, b = 0, 1
+while a < 1000:
+    if b < 1000:
+        print(a, end=',') # 取消换行改用逗号
+    else:
+        print(a)
+    a, b = b, a+b
+
---
+++ base/python_official/str.py
@@ -0,0 +1,36 @@
+# (1) """......""" 表示连续跨行输入
+#  \ 表示不需要回车换行，默认首行尾行回车换行
+print("""
+        name: yangliaoming
+        school: xidian
+        age: 21
+        """)
+
+print("""\
+        name: yangliaoming
+        school: xidian
+        age: 21\
+        """)
+
+# (2) 字符串用×或者+都可以
+print('\n' + 3 * 'niubi' + 'queshi')
+# (3) 相邻字符串自动合并
+print('\nPut several strings within parentheses '
+      'to have them joined together')
+# (4) 合并多个变量或合并变量与字符串，用'+'
+prefix = 'Py'
+print('\n' + prefix + 'thon')
+
+# (5) 字符串支持索引, 切片, 但不能直接复制赋值
+## +---+---+---+---+---+---+
+## | P | y | t | h | o | n |
+## +---+---+---+---+---+---+
+## 0   1   2   3   4   5   6
+## -6  -5  -4  -3  -2  -1
+word = 'Python'
+print('\n' + word[0], word[-1])
+print(word[0:2])  # included : excluded
+print(word[2:5])
+print(word[:2] + word[2:]) # word = word[:i] + word[i:]
+print('J' + word[1:])
+
