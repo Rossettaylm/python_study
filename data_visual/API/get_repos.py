88ffe203db65789954cc034cdf57d374ffbe8260
Rossettaylm <2441197035@qq.com>
2021-05-06 15:57:12 +0800

    finish

--- data_visual/API/get_repos.py
+++ data_visual/API/get_repos.py
@@ -6,6 +6,7 @@ r = requests.get(url)
 response_dict = r.json()
 repo_dicts = response_dict['items']
 
+
 filename = 'repos.data'
 with open(filename, 'w') as f:
     for repo_dict in repo_dicts:
@@ -15,4 +16,3 @@ with open(filename, 'w') as f:
         f.write('Repository:')
         f.write(repo_dict['html_url'])
         f.write('\n')
-    
